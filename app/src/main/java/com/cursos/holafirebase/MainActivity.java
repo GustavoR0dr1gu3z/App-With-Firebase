722e1da82e7bf4f80bb549b2b9ed74b9fdf5e35a
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2020-10-16 21:13:25 -0500

    conexion con realtime database exitosa

--- app/src/main/java/com/cursos/holafirebase/MainActivity.java
+++ app/src/main/java/com/cursos/holafirebase/MainActivity.java
@@ -1,14 +1,49 @@
 package com.cursos.holafirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PATH_START = "start";
+    private static final String PATH_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvMessage = findViewById(R.id.tvMessage);
+
+        //Variable para nuestra base de datos
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        final DatabaseReference reference = database.getReference(PATH_START).child(PATH_MESSAGE);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    tvMessage.setText(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "ERROR AL CONSULTAR EN FIREBASE", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
     }
 }
